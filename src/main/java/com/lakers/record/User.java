daeb4f5881a98f7162ef104b97f90876bf039737
Lakers <1501862490@qq.com>
2023-02-23 17:03:12 +0800

    init

--- src/main/java/com/lakers/record/User.java
+++ src/main/java/com/lakers/record/User.java
@@ -7,18 +7,21 @@ package com.lakers.record;
  */
 public class User {
 
-    private String name;
-
     public static void main(String[] args) {
-        RecordUser recordUser = new RecordUser("liuyuanlong");
-        System.out.println(recordUser.name());
+        RecordUser user1 = new RecordUser("刘元龙");
+        RecordUser user2 = new RecordUser("李浩");
+        swap(user1, user2);
+        System.out.println("person1:" + user1.name());
+        System.out.println("person2:" + user2.name());
     }
 
-    public String getName() {
-        return name;
+    public static void swap(RecordUser person1, RecordUser person2) {
+        RecordUser temp = person1;
+        person1 = person2;
+        person2 = temp;
+        System.out.println("person1:" + person1.name());
+        System.out.println("person2:" + person2.name());
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
 }
